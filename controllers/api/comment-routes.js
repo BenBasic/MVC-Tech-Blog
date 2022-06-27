2347083ab62b6eb31023401835c2ebd71142929b
BenBasic <benjaminmbasic@gmail.com>
2022-06-27 19:07:03 -0400

    Added requirements and a router get to comment-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,1 +1,17 @@
-// Will add code in here later
+// Setting up file requirements
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Gets Comment table data
+router.get('/', (req, res) => {
+    // Running the findAll method to get all comments in the table
+    Comment.findAll()
+      // Returning the result data as JSON Object
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
