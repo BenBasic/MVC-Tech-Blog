5fbc22440319b98a2b9e8a70953f2b7cf40ec288
BenBasic <benjaminmbasic@gmail.com>
2022-06-27 18:46:53 -0400

    Created route files, currently all are empty, but working on adding code to them now

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,1 @@
+// Will add code in here later
--- seeds/index.js
+++ seeds/index.js
@@ -4,3 +4,22 @@ const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 // Setting up sequelize connection requirment
 const sequelize = require('../config/connection');
+
+// Loads all seed data
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
+  
+    await seedPosts();
+    console.log('\n----- POSTS SEEDED -----\n');
+  
+    await seedComments();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+  
+    process.exit(0);
+  };
+  
+  // Calling the seedAll function
+  seedAll();
