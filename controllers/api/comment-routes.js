5c8dc29a84dccd2eda90799027714e66c6e21102
BenBasic <benjaminmbasic@gmail.com>
2022-06-27 19:13:08 -0400

    Added router post to comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,11 +7,30 @@ const withAuth = require('../../utils/auth');
 router.get('/', (req, res) => {
     // Running the findAll method to get all comments in the table
     Comment.findAll()
-      // Returning the result data as JSON Object
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
+        .then(dbCommentData => res.json(dbCommentData)) // Returning the result data as JSON Object
+        .catch(err => {
         // if there is an error, it will log an error
         console.log(err);
         res.status(500).json(err);
-      });
-  });
+    });
+});
+
+// Posts a new comment
+router.post('/', withAuth, (req, res) => {
+    // Checks if the session exits, if it does then it will create a comment
+    if (req.session) {
+        Comment.create({
+            // Creates a comment with the comment_text data while referencing the post_id to know what post to add the comment to
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // References the user_id from the current session, so it grabs the currently logged in user's user_id to assign who added the comment
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData)) // Returning the result data as JSON Object
+        .catch(err => {
+            // if there is an error, it will log an error
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
