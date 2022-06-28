acb78fab50fd85f759f7cdd519b37acb7662628e
BenBasic <benjaminmbasic@gmail.com>
2022-06-28 18:32:32 -0400

    Set up file requirements and module export in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,13 @@
-// Will add code in here later
+// Setting up file requirements
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+// Defining API route path
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+// Exports the module to be used in other files
+module.exports = router;
