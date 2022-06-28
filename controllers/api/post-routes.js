781b9e80264c02aa733f7164a8dec5023fc297b3
BenBasic <benjaminmbasic@gmail.com>
2022-06-28 17:30:56 -0400

    Added router get id to post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -26,7 +26,8 @@ router.get('/', (req, res) => {
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
+                include: 
+                {
                     model: User,
                     attributes: ['username']
                 }
@@ -40,3 +41,52 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//Get a single post by it's id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: 
+      {
+        // Checks for the request's id property, this will make it check for if the id parameter of both the request and the result match
+        id: req.params.id
+      },
+      // From the Post table, it will find all attributes including the post ID, text, title, and timestamp
+      attributes: 
+      [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      // From the User table, include the username from the User associated with the post. Then, from the Comment table, include all comments (which requires their id, comment_text, post_id, user_id, and timestamp properties)
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+      ]
+    })
+    .then(dbPostData => {
+        // If there is no matching id for the post requested, log an error
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post with this id exists' });
+          return;
+        }
+        res.json(dbPostData); // Returning the result data as JSON Object
+    })
+      .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
