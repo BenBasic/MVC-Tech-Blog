7aa00159f0c7d54b1bcfe02826ac1526c3f679c9
BenBasic <benjaminmbasic@gmail.com>
2022-06-28 21:21:20 -0400

    Added requirements to dashboard-routes, created handlebar view files

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,1 +1,6 @@
-// Will add code in here later
+// Setting up file requirements
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth')
+
